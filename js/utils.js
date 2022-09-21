3c0c623a1179be953a6a2f2bd0c567ff48ed5038
refarefa1 <114054555+refarefa1@users.noreply.github.com>
2022-09-21 10:03:39 +0300

    Day1

---
+++ js/utils.js
@@ -0,0 +1,57 @@
+'use strict'
+
+function renderBoard(mat, selector) {
+    var strHTML = '<table border="0"><tbody>'
+    for (var i = 0; i < mat.length; i++) {
+        strHTML += '<tr>'
+        for (var j = 0; j < mat[0].length; j++) {
+            const cell = mat[i][j]
+            const className = 'cell cell-' + i + '-' + j
+            strHTML += `<td class="${className}">${cell}</td>`
+        }
+        strHTML += '</tr>'
+    }
+    strHTML += '</tbody></table>'
+
+    const elContainer = document.querySelector(selector)
+    elContainer.innerHTML = strHTML
+}
+
+function countNegs(board, rowIdx, colIdx) {
+    var count = 0
+    for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+        if (i < 0 || i >= board.length) continue
+        for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+            if (i === rowIdx && j === colIdx) continue
+            if (j < 0 || j >= board[0].length) continue
+            var currCell = board[i][j]
+            if (currCell.isActive) count++
+        }
+    }
+    return count
+}
+
+function findEmptyPos(mat) {
+    var emptyPos = []
+    for (var i = 0; i < mat.length; i++) {
+        for (var j = 0; j < mat[0].length; j++) {
+            if (!mat[i][j])
+                emptyPos.push({ i: i, j: j })
+        }
+    } return emptyPos
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
